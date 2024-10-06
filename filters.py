54e2ebbdc8affb9c34f0624ae5e2edd90d20a801
GuilhermeMacedo01 <guilhermemorais586@gmail.com>
2024-10-06 15:38:12 -0300

    adding glider for high pass filter

--- filters.py
+++ filters.py
@@ -57,6 +57,28 @@ def high_pass_implemented(img_cv, canvas):
     #                       [-1,  1,  2,  1, -1],
     #                       [-1, -1, -1, -1, -1]])
 
+    filtered_img = manual_convolution(gray, kernel)
+    filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_GRAY2BGR)
+
+    display_image(filtered_img, canvas, original=False)
+
+def teste_high_pass_dinamic_kernel(img_cv, canvas, kernel_value):
+    if img_cv is None:
+        return
+
+    if not isinstance(img_cv, np.ndarray):
+        img_cv = np.array(img_cv)
+
+    gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
+    
+
+    kernel_size = kernel_value
+    if kernel_size % 2 == 0:
+        kernel_size += 1 
+
+    kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) * -1
+    kernel[kernel_size // 2, kernel_size // 2] = kernel_size ** 2 - 1
+
     filtered_img = cv2.filter2D(gray, -1, kernel)
     filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_GRAY2BGR)
 
@@ -73,7 +95,7 @@ def GuassianBlur(img: np.ndarray, sigma: Union[float, int], filter_shape: Union[
     half_x = x // 2
     half_y = y // 2
 
-    gaussian_filter = np.zeros((m, n), np.float32)
+    gaussian_filter = np.zeros((x, y), np.float32)
 
     for x in range(-half_x, half_x):
         for y in range(-half_y, half_y):
@@ -81,45 +103,28 @@ def GuassianBlur(img: np.ndarray, sigma: Union[float, int], filter_shape: Union[
             exp_term = np.exp(-(x**2.0 + y**2.0) / (2.0 * sigma**2.0))
             gaussian_filter[x + half_x, y + half_y] = normal * exp_term
 
-    blurred = convolution(img, gaussian_filter)
+    blurred = manual_convolution(img, gaussian_filter)
 
     return blurred.astype(np.uint8)
 
 
-def convolution(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    if len(image.shape) == 3:
-        m_i, n_i, c_i = image.shape
-    elif len(image.shape) == 2:
-        image = image[..., np.newaxis]
-        m_i, n_i, c_i = image.shape
-    else:
-        raise Exception('shape not supported')
 
-    m_k, n_k = kernel.shape
-    y_strides = m_i - m_k + 1
-    x_strides = n_i - n_k + 1
+def manual_convolution(image, kernel):
 
-    img = image.copy()
-    output_shape = (m_i - m_k + 1, n_i - n_k + 1, c_i)
-    output = np.zeros(output_shape, dtype=np.float32)
+    image_height, image_width = image.shape
+    kernel_height, kernel_width = kernel.shape
 
-    count = 0
-    output_tmp = output.reshape((output_shape[0] * output_shape[1], output_shape[2]))
-
-    for y in range(y_strides):
-        for x in range(x_strides):
-            for c in range(c_i):
-                sub_matrix = img[y:y + m_k, x:x + n_k, c]
-                output_tmp[count, c] = np.sum(sub_matrix * kernel)
-            count += 1
-
-    return output_tmp.reshape(output_shape)
+    output = np.zeros_like(image)
 
 
-def plot(data, title):
-    plot.i += 1
-    plt.subplot(2,2,plot.i)
-    plt.imshow(data)
-    plt.gray()
-    plt.title(title)
+    pad_h = kernel_height // 2
+    pad_w = kernel_width // 2
 
+    for i in range(pad_h, image_height - pad_h):
+        for j in range(pad_w, image_width - pad_w):
+            sub_matrix = image[i - pad_h:i + pad_h + 1, j - pad_w:j + pad_w + 1]
+            output[i, j] = np.sum(sub_matrix * kernel)
+    
+    output = np.clip(output, 0, 255)
+    
+    return output.astype(np.uint8)
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog
 from image_handler import load_image, display_image
-from filters import low_pass, high_pass, low_pass_implemented, high_pass_implemented
+from filters import low_pass, high_pass, low_pass_implemented, high_pass_implemented, teste_high_pass_dinamic_kernel
 
 img_cv = None
 
 def set_img_cv(img):
     global img_cv
     img_cv = img
+
+def high_pass_slider(root, img_cv, canvas):
+    def on_slider_change(value):
+        kernel_value = int(value)
+        teste_high_pass_dinamic_kernel(img_cv, canvas, kernel_value)
+    
+    slider = tk.Scale(root, from_=1, to=15, orient=tk.HORIZONTAL, label="Kernel", command=on_slider_change)
+    slider.grid(row=0, column=0, padx=50, pady=50)
+    slider.set(3)
 
 root = tk.Tk()
 root.title("Image Processing App")
@@ -30,6 +39,7 @@ filters_menu.add_command(label="Low Pass Filter", command=lambda: low_pass(img_c
 filters_menu.add_command(label="Low Pass Filter Implemented", command=lambda: low_pass_implemented(img_cv, edited_image_canvas))
 filters_menu.add_command(label="High Pass Filter", command=lambda: high_pass(img_cv, edited_image_canvas))
 filters_menu.add_command(label="High Pass Filter Implemented", command=lambda: high_pass_implemented(img_cv, edited_image_canvas))
+filters_menu.add_command(label="High Pass Filter with Slider", command=lambda: high_pass_slider(root, img_cv, edited_image_canvas))
 
 
 original_image_canvas = tk.Canvas(root, width=500, height=500, bg="#2e2e2e", highlightthickness=1, highlightbackground="white")
