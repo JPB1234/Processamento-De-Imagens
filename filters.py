5f534bacfd68571385cb0f2955c272bd153b9e69
Joao Paulo <jpboechat11@gmail.com>
2024-12-09 19:17:27 -0300

    a

--- filters.py
+++ filters.py
@@ -3,7 +3,6 @@ from typing import Union, List, Tuple
 import numpy as np
 from image_handler import display_image
 
-edited_img = any
 
 def thresholding_segmentation(img_cv, canvas):
     if img_cv is None:
@@ -12,8 +11,8 @@ def thresholding_segmentation(img_cv, canvas):
     gray_img = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
     binary_img = aplying_thresholding(gray_img)
 
-    edited_img = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    binary_img_bgr = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
+    display_image(binary_img_bgr, canvas, original=False)
 
 
 def otsu_segmentation(img_cv, canvas):
@@ -24,16 +23,16 @@ def otsu_segmentation(img_cv, canvas):
 
     binary_img = aplying_otsu(gray_img)
 
-    edited_img = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    otsu_img_bgr = cv2.cvtColor(binary_img, cv2.COLOR_GRAY2BGR)
+    display_image(otsu_img_bgr, canvas, original=False)
 
 def erosion(img_cv, canvas, kernel_size = 5):
     if img_cv is None:
         return
     img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
     output_img = aplying_erosion(img_cv,kernel_size)
-    edited_img = cv2.cvtColor(output_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    eroded_img = cv2.cvtColor(output_img, cv2.COLOR_GRAY2BGR)
+    display_image(eroded_img, canvas, original=False)
 
 
 def dilatation(img_cv, canvas, kernel_size=5):
@@ -42,8 +41,8 @@ def dilatation(img_cv, canvas, kernel_size=5):
     img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
     output_img = aplying_dilatation(img_cv,kernel_size)
     
-    edited_img = cv2.cvtColor(output_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    dilated_img = cv2.cvtColor(output_img, cv2.COLOR_GRAY2BGR)
+    display_image(dilated_img, canvas, original=False)
 
 def open(img_cv, canvas, kernel_size=5):
     if img_cv is None:
@@ -53,8 +52,8 @@ def open(img_cv, canvas, kernel_size=5):
     erosion_img = aplying_erosion(segment_img,kernel_size)
     dilated_img = aplying_dilatation(erosion_img,kernel_size)
     
-    edited_img = cv2.cvtColor(dilated_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False) 
+    final_img = cv2.cvtColor(dilated_img, cv2.COLOR_GRAY2BGR)
+    display_image(final_img, canvas, original=False) 
 
 def close(img_cv, canvas, kernel_size=5):
     if img_cv is None:
@@ -64,15 +63,15 @@ def close(img_cv, canvas, kernel_size=5):
     dilated_img = aplying_dilatation(segment_img,kernel_size)
     erosion_img = aplying_erosion(dilated_img,kernel_size)
     
-    edited_img = cv2.cvtColor(erosion_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False) 
+    final_img = cv2.cvtColor(erosion_img, cv2.COLOR_GRAY2BGR)
+    display_image(final_img, canvas, original=False) 
 
     
 def low_pass(img_cv, canvas):
     if img_cv is None:
         return
-    edited_img = cv2.GaussianBlur(img_cv, (15, 15), 0)
-    display_image(edited_img, canvas, original=False)
+    filtered_img = cv2.GaussianBlur(img_cv, (15, 15), 0)
+    display_image(filtered_img, canvas, original=False)
 
 def low_pass_gaussian(img_cv, canvas, sigma=3.0):
     if img_cv is None:
@@ -81,11 +80,11 @@ def low_pass_gaussian(img_cv, canvas, sigma=3.0):
     if not isinstance(img_cv, np.ndarray):
         img_cv = np.array(img_cv)
 
-    edited_img = GuassianBlur(img_cv, sigma)
-    if not isinstance(edited_img, np.ndarray):
+    filtered_img = GuassianBlur(img_cv, sigma)
+    if not isinstance(filtered_img, np.ndarray):
         raise TypeError("not nparray")
     
-    display_image(edited_img, canvas, original=False)
+    display_image(filtered_img, canvas, original=False)
 
 def low_pass_media(img_cv, canvas):
     if img_cv is None:
@@ -94,20 +93,20 @@ def low_pass_media(img_cv, canvas):
     if not isinstance(img_cv, np.ndarray):
         img_cv = np.array(img_cv)
 
-    edited_img = low_pass_mean_filter(img_cv)
-    if not isinstance(edited_img, np.ndarray):
+    filtered_img = low_pass_mean_filter(img_cv)
+    if not isinstance(filtered_img, np.ndarray):
         raise TypeError("not nparray")
     
-    display_image(edited_img, canvas, original=False)
+    display_image(filtered_img, canvas, original=False)
 
 def high_pass(img_cv, canvas):
     if img_cv is None:
         return
     gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
-    edited_img = cv2.Laplacian(gray, cv2.CV_64F)
-    edited_img = cv2.convertScaleAbs(edited_img)
-    edited_img = cv2.cvtColor(edited_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    filtered_img = cv2.Laplacian(gray, cv2.CV_64F)
+    filtered_img = cv2.convertScaleAbs(filtered_img)
+    filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_GRAY2BGR)
+    display_image(filtered_img, canvas, original=False)
 
 def high_pass_laplacian(img_cv, canvas, kernel_value):
     if img_cv is None:
@@ -124,9 +123,9 @@ def high_pass_laplacian(img_cv, canvas, kernel_value):
     kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) * -1
     kernel[kernel_size // 2, kernel_size // 2] = kernel_size ** 2 - 1
 
-    edited_img = cv2.filter2D(gray, -1, kernel)
-    edited_img = cv2.cvtColor(edited_img, cv2.COLOR_GRAY2BGR)
-    display_image(edited_img, canvas, original=False)
+    filtered_img = cv2.filter2D(gray, -1, kernel)
+    filtered_img = cv2.cvtColor(filtered_img, cv2.COLOR_GRAY2BGR)
+    display_image(filtered_img, canvas, original=False)
 
 def high_pass_sobel(img_cv, canvas):
     if img_cv is None:
@@ -135,8 +134,8 @@ def high_pass_sobel(img_cv, canvas):
     if not isinstance(img_cv, np.ndarray):
         img_cv = np.array(img_cv)
     
-    edited_img = sobel_filter_manual(img_cv)
-    display_image(edited_img, canvas, original=False)
+    filtered_img = sobel_filter_manual(img_cv)
+    display_image(filtered_img, canvas, original=False)
 
 def GuassianBlur(img: np.ndarray, sigma: Union[float, int], filter_shape: Union[List, Tuple, None] = None):
     if filter_shape is None:
@@ -157,26 +156,26 @@ def GuassianBlur(img: np.ndarray, sigma: Union[float, int], filter_shape: Union[
             exp_term = np.exp(-(x**2.0 + y**2.0) / (2.0 * sigma**2.0))
             gaussian_filter[x + half_x, y + half_y] = normal * exp_term
 
-    edited_img = cv2.filter2D(img, -1, gaussian_filter)
-    return edited_img.astype(np.uint8)
+    blurred = cv2.filter2D(img, -1, gaussian_filter)
+    return blurred.astype(np.uint8)
 
 def manual_convolution(image, kernel):
     image_height, image_width = image.shape
     kernel_height, kernel_width = kernel.shape
-    edited_img = np.zeros_like(image)
+    output = np.zeros_like(image)
     pad_h = kernel_height // 2
     pad_w = kernel_width // 2
     for i in range(pad_h, image_height - pad_h):
         for j in range(pad_w, image_width - pad_w):
             sub_matrix = image[i - pad_h:i + pad_h + 1, j - pad_w:j + pad_w + 1]
-            edited_img[i, j] = np.sum(sub_matrix * kernel)
-    edited_img = np.clip(edited_img, 0, 255)
-    return edited_img.astype(np.uint8)
+            output[i, j] = np.sum(sub_matrix * kernel)
+    output = np.clip(output, 0, 255)
+    return output.astype(np.uint8)
 
 def low_pass_mean_filter(img, kernel_size=9):
     kernel = np.ones((kernel_size, kernel_size), np.float32) / (kernel_size * kernel_size)
-    edited_img = cv2.filter2D(img, -1, kernel)
-    return edited_img
+    filtered_img = cv2.filter2D(img, -1, kernel)
+    return filtered_img
 
 def sobel_filter_manual(img, direction='x'):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -184,32 +183,32 @@ def sobel_filter_manual(img, direction='x'):
             kernel = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32)  # Sobel X
     elif direction == 'y':
         kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32)  # Sobel Y
-    edited_img = cv2.filter2D(gray, cv2.CV_64F, kernel)
+    sobel_img = cv2.filter2D(gray, cv2.CV_64F, kernel)
 
-    return cv2.convertScaleAbs(edited_img)
+    return cv2.convertScaleAbs(sobel_img)
 
 def aplying_erosion(img_cv, kernel_size = 5):
     pad_size = kernel_size // 2
     padded_img = np.pad(img_cv, pad_size, mode='constant', constant_values=255)
-    edited_img = np.zeros_like(img_cv)
+    output_img = np.zeros_like(img_cv)
 
     for i in range(pad_size, padded_img.shape[0] - pad_size):
         for j in range(pad_size, padded_img.shape[1] - pad_size):
             region = padded_img[i - pad_size:i + pad_size + 1, j - pad_size:j + pad_size + 1]
-            edited_img[i - pad_size, j - pad_size] = np.min(region)
-    return edited_img
+            output_img[i - pad_size, j - pad_size] = np.min(region)
+    return output_img
 
 def aplying_dilatation(img_cv, kernel_size = 5):
     pad_size = kernel_size // 2
     padded_img = np.pad(img_cv, pad_size, mode='constant', constant_values=0)
     
-    edited_img = np.zeros_like(img_cv)
+    output_img = np.zeros_like(img_cv)
 
     for i in range(pad_size, padded_img.shape[0] - pad_size):
         for j in range(pad_size, padded_img.shape[1] - pad_size):
             region = padded_img[i - pad_size:i + pad_size + 1, j - pad_size:j + pad_size + 1]
-            edited_img[i - pad_size, j - pad_size] = np.max(region)
-    return edited_img
+            output_img[i - pad_size, j - pad_size] = np.max(region)
+    return output_img
 
 def aplying_otsu(gray_img):
     hist, bins = np.histogram(gray_img.flatten(), 256, [0, 256])
@@ -242,15 +241,15 @@ def aplying_otsu(gray_img):
             current_max = between_class_variance
             threshold_value = i
 
-    edited_img = np.zeros_like(gray_img)
-    edited_img[gray_img >= threshold_value] = 255
-    return edited_img
+    otsu_img = np.zeros_like(gray_img)
+    otsu_img[gray_img >= threshold_value] = 255
+    return otsu_img
 
 def aplying_thresholding(gray_img):
     
     threshold_value = 90
-    edited_img = np.zeros_like(gray_img)
+    binary_img = np.zeros_like(gray_img)
     
-    edited_img[gray_img >= threshold_value] = 255
+    binary_img[gray_img >= threshold_value] = 255
 
-    return edited_img
+    return binary_img
